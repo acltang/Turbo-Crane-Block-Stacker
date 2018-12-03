9b16454712245195ec3a06f19ae76ccbf7a6e631
actang <act.alextang@gmail.com>
2018-12-03 01:13:07 -0800

    fix block grabbing

--- Assets/Source/Model/Block.cs
+++ Assets/Source/Model/Block.cs
@@ -23,7 +23,7 @@ public class Block : MonoBehaviour {
 		mD = Vector3.Dot(TopAnchor, transform.up);
 		Vector3 p = TheWorld.HookPosition - transform.up * (Vector3.Dot(TheWorld.HookPosition, transform.up) - mD);
 		Vector3 d = p - TopAnchor;
-		if (d.magnitude < AnchorRadius && TheWorld.HookPosition.y <= TopAnchor.y) {
+		if (d.magnitude < AnchorRadius && (TheWorld.HookPosition.y - TopAnchor.y) <= 0f && (TheWorld.HookPosition.y - TopAnchor.y) > -0.25f) {
 			picked = true;
 			TheWorld.HasBlock = true;
 			GetComponent<Renderer>().material.color = Color.black;
